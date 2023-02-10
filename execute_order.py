ba8fd420202d217b25b6406777336e122dbd4fc3
Lau Jing Xuan <melvinlaujx@gmail.com>
2023-02-10 22:57:51 +0800

    minor changes

--- execute_order.py
+++ execute_order.py
@@ -7,9 +7,27 @@ import requests
 
 #API for create new order: https://binance-docs.github.io/apidocs/delivery/en/#new-order-trade
 #TRADE	endpoint requires sending a valid API-Key and signature.
-#When a 429 is received, it's your obligation as an API to back off and not spam the API. =====> Need to sleep before calculate and retry again?
 HTTP_TOO_MANY_REQUESTS = 429
+HTTP_TIME_OUT=408
 SLEEP_TIME = 5
+
+# TODO at main flow: When a 429 is received, it's your obligation as an API to back off and not spam the API (that's why sleep )
+# if resp.status_code == HTTP_TOO_MANY_REQUESTS:
+#     asyncio.sleep(SLEEP_TIME)
+# return
+async def execute_order(config, symbol, price, quantity):
+    endpoint = f"{config['http_base_url_live']}/dapi/v1/order"
+    data = createDataAndSignature(config['secure_key'], symbol, price, quantity)
+    headers = {"Content-Type": "application/x-www-form-urlencoded", "X-MBX-APIKEY":config['api_key']}
+
+    try:
+        resp = requests.post(url = endpoint, headers=headers, data = data, timeout=10)
+        print(resp.content)
+        return resp.status_code
+    except requests.Timeout as error:
+        #if timeout then just return
+        print("Order execution failed due to timeout: " + error)
+        return HTTP_TIME_OUT
 
 def hashing(secretKey, query_string):
     return hmac.new(
@@ -26,7 +44,7 @@ def createDataAndSignature(secretKey, symbol, price, quantity):
     timestamp = int(time.time() * 1000)
     signatureString = f"symbol={symbol}&side={side}&type={type}&quantity={quantity}&price={price}&timeInForce={timeInForce}&timestamp={timestamp}"
     signature = hashing(secretKey, signatureString)
-    
+
     data = {
         "symbol":symbol,
         "side":side,
@@ -38,22 +56,6 @@ def createDataAndSignature(secretKey, symbol, price, quantity):
         "signature":signature
     }
     return data
-
-async def execute_order(config, symbol, price, quantity):
-    endpoint = f"{config['http_base_url_live']}/dapi/v1/order"
-    data = createDataAndSignature(config['secure_key'], symbol, price, quantity)
-    headers = {"Content-Type": "application/x-www-form-urlencoded", "X-MBX-APIKEY":config['api_key']}
-
-    try:
-        resp = requests.post(url = endpoint, headers=headers, data = data, timeout=10)
-        if resp.status_code == HTTP_TOO_MANY_REQUESTS:
-            asyncio.sleep(SLEEP_TIME)
-            return
-        print(resp.content)
-    except requests.Timeout as error:
-        #if timeout then just return
-        print("Order execution failed due to timeout: " + error)
-        return
 
 async def test():
     f = open('config.json')
