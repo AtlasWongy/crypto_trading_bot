abe95d6710b61f9feee5846e80d451e179bedee1
AtlasWongy <atlasyjyj@gmail.com>
2023-03-25 15:34:29 +0800

    Update execute_order.py
    Clean up code and implemented take profit and stop loss

--- execute_order.py
+++ execute_order.py
@@ -2,7 +2,6 @@ import asyncio
 import hashlib
 import hmac
 import json
-import time
 import requests
 import websockets
 
@@ -25,38 +24,7 @@ async def byte_unpack_loader(response_in_bytes):
     return response_dict
 
 
-async def check_order_status(config, symbol, order_id):
-    endpoint = f"{config['http_base_url_test']}/fapi/v1/openOrder"
-    data = await createDataAndSignature(
-        config=config,
-        secretKey=config['test_net_futures']['secure_key'],
-        symbol=symbol,
-        order_id=order_id,
-        execution_type='CHECK_ORDER'
-    )
-
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "X-MBX-APIKEY": config['test_net_futures']['api_key']
-    }
-
-    try:
-        response = requests.get(url=endpoint, headers=headers, params=data)
-        response_unloaded = await byte_unpack_loader(response.content)
-        print("The response: ", response_unloaded)
-        order_status = response_unloaded['status']
-
-        print("The order status is: ", order_status)
-
-        return order_status
-    except (requests.Timeout, KeyError) as error:
-        # if timeout then just return
-        # print("Order execution failed due to timeout: " + error)
-        print("Error due to: ", error)
-        return HTTP_TIME_OUT
-
-
-async def check_order_status_socket(config):
+async def order_handler_websocket(config):
     get_listen_key_endpoint = f"{config['http_base_url_test']}/fapi/v1/listenKey"
     response = requests.post(
         url=get_listen_key_endpoint,
@@ -77,11 +45,33 @@ async def check_order_status_socket(config):
                     data = json.loads(message)
                     try:
                         if data['e'] == 'ORDER_TRADE_UPDATE':
-                            print(data)
-                        #     if data['o']['X'] == 'FILLED':
-                        #         print('SEND THE TAKE PROFIT AND STOP LOSS')
-                        #     else:
-                        #         print('Order is not fulfilled...')
+                            # print(data)
+                            if data['o']['X'] == 'FILLED':
+                                current_server_time = await check_server_time(config)
+                                await execute_order_take_profit_stop(
+                                    config=config,
+                                    symbol="ETHUSDT",
+                                    price=1780,
+                                    quantity=1,
+                                    current_server_time=current_server_time,
+                                    side="SELL",
+                                    execution_type="TAKE_PROFIT",
+                                    stop_price=1795
+
+                                )
+                                await execute_order_take_profit_stop(
+                                    config=config,
+                                    symbol="ETHUSDT",
+                                    price=1780,
+                                    quantity=1,
+                                    current_server_time=current_server_time,
+                                    side="SELL",
+                                    execution_type="STOP",
+                                    stop_price=1748
+                                )
+                                print("TAKE PROFIT and STOP LOSS fulfilled")
+                            else:
+                                print('Order is not fulfilled...')
 
                     except KeyError:
                         print("Faulty data received from API")
@@ -111,9 +101,9 @@ async def ping_server(config) -> None:
 
 
 async def execute_order(config, symbol, price, quantity, current_server_time, side, execution_type):
-    await asyncio.sleep(0.45)
+    await asyncio.sleep(0.5)
     endpoint = f"{config['http_base_url_test']}/fapi/v1/order"
-    data = await createDataAndSignature(
+    data = await create_data_and_signature(
         config=config,
         secretKey=config['test_net_futures']['secure_key'],
         symbol=symbol,
@@ -144,17 +134,50 @@ async def execute_order(config, symbol, price, quantity, current_server_time, si
         return HTTP_TIME_OUT
 
 
+async def execute_order_take_profit_stop(config, symbol, price, quantity, current_server_time,
+                                         side, execution_type, stop_price):
+    endpoint = f"{config['http_base_url_test']}/fapi/v1/order"
+    data = await create_data_and_signature(
+        config=config,
+        secretKey=config['test_net_futures']['secure_key'],
+        symbol=symbol,
+        price=price,
+        quantity=quantity,
+        server_time=current_server_time,
+        side=side,
+        execution_type=execution_type,
+        stop_price=stop_price
+    )
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-MBX-APIKEY": config['test_net_futures']['api_key']
+    }
+
+    try:
+        resp = requests.post(url=endpoint, headers=headers, data=data, timeout=10)
+        print("The status code: ", resp.status_code)
+        response_unloaded = await byte_unpack_loader(resp.content)
+        print(response_unloaded)
+        # order_id = response_unloaded['orderId']
+        return
+        # return resp.status_code, order_id
+    except requests.Timeout as error:
+        # if timeout then just return
+        print("Order execution failed due to timeout: " + error)
+        return HTTP_TIME_OUT
+
+
 async def hashing(secret_key, query_string):
     return hmac.new(
         secret_key.encode("utf-8"), query_string.encode("utf-8"), hashlib.sha256
     ).hexdigest()
 
 
-async def createDataAndSignature(
-        config, secretKey, symbol, price=0, quantity=0, server_time=0, side='', execution_type='', order_id='0'):
+async def create_data_and_signature(
+        config, secretKey, symbol, price=0, quantity=0, server_time=0, side='',
+        execution_type='', order_id='0', stop_price='0'):
     if execution_type == "LIMIT":
-
-        print("The current server time: ", server_time)
 
         symbol = symbol
         side = side
@@ -192,7 +215,31 @@ async def createDataAndSignature(
         }
 
         return data
+    elif execution_type == "TAKE_PROFIT" or execution_type == "STOP":
 
+        symbol = symbol
+        side = side
+        type = execution_type
+        timeInForce = 'GTC'
+        price = price
+        quantity = quantity
+        timestamp = server_time
+        signatureString = f"symbol={symbol}&side={side}&type={type}&quantity={quantity}&price={price}&timeInForce={timeInForce}&timestamp={timestamp}&stopPrice={stop_price}"
+        signature = await hashing(secretKey, signatureString)
+
+        data = {
+            "symbol": symbol,
+            "side": side,
+            "type": type,
+            "quantity": quantity,
+            "price": price,
+            "timeInForce": timeInForce,
+            "timestamp": timestamp,
+            "signature": signature,
+            "stopPrice": stop_price
+        }
+
+        return data
     else:
         return
 
@@ -203,33 +250,15 @@ async def test():
     current_server_time = await check_server_time(config)
     await ping_server(config)
 
-    # Execute
-    # status_code, order_id = await execute_order(config, "ETHUSDT", 1470, 1, current_server_time, "BUY", "LIMIT")
     execute_order_task = asyncio.create_task(
-        execute_order(config, "ETHUSDT", 1470, 1, current_server_time, "BUY", "LIMIT")
+        execute_order(config, "ETHUSDT", 1780, 1, current_server_time, "BUY", "LIMIT")
     )
 
-    # Check if order is finished
-    # while await check_order_status(config=config, symbol="ETHUSDT", order_id=order_id) != "FILLED":
-    #     time.sleep(5)
-    #     print("order is not fulfilled....")
-    #     continue
-    # Web Socket
-    # Yi Jie
-    # await check_order_status_socket(config)
     check_order_status_task = asyncio.create_task(
-        check_order_status_socket(config)
+        order_handler_websocket(config)
     )
-    #
+
     await asyncio.gather(execute_order_task, check_order_status_task)
-
-    # Testing Git
-
-    # Stop
-    # Hui Wen
-
-    # Take Profit
-    # Hui Wen
 
 
 if __name__ == "__main__":
